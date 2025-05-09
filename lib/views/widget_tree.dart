0da36a2d5b8bcc1cf99ae368d8859d17c53487d2
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-05-09 15:35:00 +0200

    move file location

--- lib/views/widget_tree.dart
+++ lib/views/widget_tree.dart
@@ -1,5 +1,9 @@
-import 'package:first_project/widgets/navbar_widget.dart';
+import 'package:first_project/views/pages/home_page.dart';
+import 'package:first_project/views/pages/profile_page.dart';
+import 'package:first_project/views/widgets/navbar_widget.dart';
 import 'package:flutter/material.dart';
+
+List<Widget> pages = [HomePage(), ProfilePage()];
 
 class WidgetTree extends StatelessWidget {
   const WidgetTree({super.key});
@@ -8,6 +12,7 @@ class WidgetTree extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Flutter'), centerTitle: true),
+      body: pages.elementAt(1),
       bottomNavigationBar: NavbarWidget(),
     );
   }
