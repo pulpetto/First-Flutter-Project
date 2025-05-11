6a539410d3d5d3c95cfe9651f7103b2b62c7cc24
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-05-11 12:30:00 +0200

    add pages switching

--- lib/views/widget_tree.dart
+++ lib/views/widget_tree.dart
@@ -1,3 +1,4 @@
+import 'package:first_project/data/notifiers.dart';
 import 'package:first_project/views/pages/home_page.dart';
 import 'package:first_project/views/pages/profile_page.dart';
 import 'package:first_project/views/widgets/navbar_widget.dart';
@@ -12,7 +13,12 @@ class WidgetTree extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Flutter'), centerTitle: true),
-      body: pages.elementAt(1),
+      body: ValueListenableBuilder(
+        valueListenable: selectedPageNotifier,
+        builder: (context, selectedPage, child) {
+          return pages.elementAt(selectedPage);
+        },
+      ),
       bottomNavigationBar: NavbarWidget(),
     );
   }
--- lib/views/widgets/navbar_widget.dart
+++ lib/views/widgets/navbar_widget.dart
@@ -1,28 +1,25 @@
+import 'package:first_project/data/notifiers.dart';
 import 'package:flutter/material.dart';
 
-class NavbarWidget extends StatefulWidget {
+class NavbarWidget extends StatelessWidget {
   const NavbarWidget({super.key});
 
   @override
-  State<NavbarWidget> createState() => _NavbarWidgetState();
-}
-
-class _NavbarWidgetState extends State<NavbarWidget> {
-  int currentIndex = 0;
-
-  @override
   Widget build(BuildContext context) {
-    return NavigationBar(
-      destinations: [
-        NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-        NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
-      ],
-      onDestinationSelected: (int value) {
-        setState(() {
-          currentIndex = value;
-        });
+    return ValueListenableBuilder(
+      valueListenable: selectedPageNotifier,
+      builder: (context, selectedPage, child) {
+        return NavigationBar(
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+          onDestinationSelected: (int value) {
+            selectedPageNotifier.value = value;
+          },
+          selectedIndex: selectedPage,
+        );
       },
-      selectedIndex: currentIndex,
     );
   }
 }
