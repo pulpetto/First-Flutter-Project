85197da0c324334b789f3913733cc99e08e23ede
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-05-12 06:57:18 +0200

    pass title in routing

--- lib/views/widget_tree.dart
+++ lib/views/widget_tree.dart
@@ -24,7 +24,7 @@ class WidgetTree extends StatelessWidget {
                 context,
                 MaterialPageRoute(
                   builder: (context) {
-                    return SettingsPage();
+                    return SettingsPage(title: 'Settings x');
                   },
                 ),
               );
