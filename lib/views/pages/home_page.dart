2f48ee89921e5611504ce486911bcee220f2f391
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-05-14 06:11:23 +0200

    add style variables

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:first_project/data/constants.dart';
 import 'package:first_project/views/widgets/hero_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -8,7 +9,30 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(20.0),
-      child: Column(children: [HeroWidget()]),
+      child: Column(
+        children: [
+          HeroWidget(),
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(vertical: 10.0),
+            child: Card(
+              child: Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Basic Layout', style: KTextStyle.titleTealText),
+                    Text(
+                      'The description of this',
+                      style: KTextStyle.descriptionText,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
