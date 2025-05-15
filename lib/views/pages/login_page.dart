a28dfc95fb58840486155c1ec15f9e4d2f3d26af
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-05-15 09:03:23 +0200

    cosmetic changes

--- lib/views/pages/login_page.dart
+++ lib/views/pages/login_page.dart
@@ -26,48 +26,54 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            HeroWidget(title: 'Login'),
-            SizedBox(height: 20.0),
-            TextField(
-              controller: controllerEmail,
-              decoration: InputDecoration(
-                hintText: 'Email',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15.0),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                HeroWidget(title: 'Login'),
+                SizedBox(height: 20.0),
+                TextField(
+                  controller: controllerEmail,
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                  ),
+                  onEditingComplete: () {
+                    setState(() {});
+                  },
                 ),
-              ),
-              onEditingComplete: () {
-                setState(() {});
-              },
-            ),
-            SizedBox(height: 10.0),
-            TextField(
-              controller: controllerPassword,
-              decoration: InputDecoration(
-                hintText: 'Password',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15.0),
+                SizedBox(height: 10.0),
+                TextField(
+                  controller: controllerPassword,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                  ),
+                  onEditingComplete: () {
+                    setState(() {});
+                  },
                 ),
-              ),
-              onEditingComplete: () {
-                setState(() {});
-              },
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    onLoginPressed();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(double.infinity, 40.0),
+                  ),
+                  child: Text('Login'),
+                ),
+                SizedBox(height: 50.0),
+              ],
             ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                onLoginPressed();
-              },
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size(double.infinity, 40.0),
-              ),
-              child: Text('Login'),
-            ),
-          ],
+          ),
         ),
       ),
     );
