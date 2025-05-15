bf0c29a57a6d5b2df84c6bcccc162dfc5c05d9af
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-05-15 11:14:58 +0200

    backwards navigation fix

--- lib/views/pages/login_page.dart
+++ lib/views/pages/login_page.dart
@@ -84,13 +84,14 @@ class _LoginPageState extends State<LoginPage> {
   void onLoginPressed() {
     if (confirmedEmail == controllerEmail.text &&
         confirmedPassword == controllerPassword.text) {
-      Navigator.pushReplacement(
+      Navigator.pushAndRemoveUntil(
         context,
         MaterialPageRoute(
           builder: (context) {
             return WidgetTree();
           },
         ),
+        (route) => false,
       );
     }
   }
